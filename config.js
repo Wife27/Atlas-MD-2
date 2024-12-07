4a43d180e34145766a4657a8a951695160dd0bbe
Wife27 <noenmike590@gmail.com>
2024-12-07 02:03:07 -0400

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "18294797498";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
